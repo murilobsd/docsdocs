15c38c6c8ac2e75a04931757801d02a4278dfa20
murilobsd <murilo@lab804.com.br>
2019-01-31 22:13:02 -0200

    exporting public and private key

--- src/docsdocs/crypto/generate.go
+++ src/docsdocs/crypto/generate.go
@@ -3,13 +3,19 @@ package crypto
 import (
 	"crypto/rand"
 	"crypto/rsa"
+	"crypto/x509"
 	"docsdocs/log"
 	"encoding/gob"
+	"encoding/pem"
 	"errors"
+	"io"
 )
 
 var (
-	errCreatingKey = errors.New("creating key fail")
+	errCreatingKey        = errors.New("creating key fail")
+	errExportPrivateKey   = errors.New("failed to encode private key")
+	errSerializePublicKey = errors.New("failed to serializing public key")
+	errExportPublicKey    = errors.New("failed to encode public key")
 )
 
 func init() {
@@ -23,10 +29,10 @@ const RSAKeySize int = 2048
 type Encrypter interface {
 	// Encode(*rsa.PublicKey) ([]byte, error)
 	// Decode([]byte) (*rsa.PublicKey, error)
-	// WritePrivate() error
-	// WritePublic() error
+	ExportPrivate(io.Writer) error
+	ExportPublic(io.Writer) error
 	GetPrivate() *rsa.PrivateKey
-	GetPublic() rsa.PublicKey
+	GetPublic() *rsa.PublicKey
 	// ReadPrivate() (*rsa.PrivateKey, error)
 	// ReadPublic() (*rsa.PublicKey, error)
 }
@@ -44,12 +50,12 @@ func CreateKey() (*rsa.PrivateKey, error) {
 
 // NewCrypto ...
 func NewCrypto() (Encrypter, error) {
-	priKey, err := CreateKey()
+	privKey, err := CreateKey()
 	if err != nil {
 		return nil, errCreatingKey
 	}
 	docCrypto := &DocsCrypto{
-		privateKey: priKey,
+		privateKey: privKey,
 		Logger:     log.NewDocsLogger(),
 	}
 	return docCrypto, nil
@@ -62,7 +68,39 @@ func (d *DocsCrypto) GetPrivate() *rsa.PrivateKey {
 }
 
 // GetPublic return public key
-func (d *DocsCrypto) GetPublic() rsa.PublicKey {
+func (d *DocsCrypto) GetPublic() *rsa.PublicKey {
 	d.Debug("gettting public key")
-	return d.privateKey.PublicKey
+	return d.privateKey.Public().(*rsa.PublicKey)
+}
+
+// ExportPrivate convert a private to pem and storage to disk
+func (d *DocsCrypto) ExportPrivate(out io.Writer) error {
+	d.Info("Exporting private key")
+	if err := pem.Encode(out, &pem.Block{
+		Type:  "PRIVATE KEY",
+		Bytes: x509.MarshalPKCS1PrivateKey(d.GetPrivate()),
+	}); err != nil {
+		d.Error(err)
+		return errExportPrivateKey
+	}
+	return nil
+}
+
+// ExportPublic convert a public key to pem and storage to disk
+func (d *DocsCrypto) ExportPublic(out io.Writer) error {
+	d.Info("Exporting public key")
+	pubKey, err := x509.MarshalPKIXPublicKey(d.GetPublic())
+	d.Debug("Serializing private key")
+	if err != nil {
+		d.Error(err)
+		return errSerializePublicKey
+	}
+	if err = pem.Encode(out, &pem.Block{
+		Type:  "PUBLIC KEY",
+		Bytes: pubKey,
+	}); err != nil {
+		d.Error(err)
+		return errExportPublicKey
+	}
+	return nil
 }
--- src/docsdocs/crypto/generate_test.go
+++ src/docsdocs/crypto/generate_test.go
@@ -1,13 +1,27 @@
 package crypto
 
 import (
+	"bytes"
+	"fmt"
 	"testing"
 )
+
+func TestCreateKey(t *testing.T) {
+	size := 2048
+	key, err := CreateKey()
+	if err != nil {
+		t.Error(err)
+	}
+
+	if bits := key.N.BitLen(); bits != size {
+		t.Errorf("private key too short (%d vs %d)", bits, size)
+	}
+}
 
 func TestNewCrypto(t *testing.T) {
 	_, err := NewCrypto()
 	if err != nil {
-		t.Errorf("failed new crypto")
+		t.Error(err)
 	}
 
 }
@@ -16,11 +30,11 @@ func TestGetPrivate(t *testing.T) {
 	size := 2048
 	docCrypto, err := NewCrypto()
 	if err != nil {
-		t.Errorf("failed new crypto")
+		t.Error(err)
 	}
 	privKey := docCrypto.GetPrivate()
 	if docCrypto.GetPrivate() == nil {
-		t.Errorf("private key not exist")
+		t.Error("failed new crypto")
 	}
 	if bits := privKey.N.BitLen(); bits != size {
 		t.Errorf("private key too short (%d vs %d)", bits, size)
@@ -30,10 +44,35 @@ func TestGetPrivate(t *testing.T) {
 func TestGetPublic(t *testing.T) {
 	docCrypto, err := NewCrypto()
 	if err != nil {
-		t.Errorf("failed new crypto")
+		t.Error(err)
 	}
 
 	if docCrypto.GetPublic().E == 0 {
 		t.Errorf("public key not exist")
 	}
+}
+
+func TestExportPrivate(t *testing.T) {
+	docCrypto, err := NewCrypto()
+	if err != nil {
+		t.Error(err)
+	}
+	buf := &bytes.Buffer{}
+	err = docCrypto.ExportPrivate(buf)
+	if err != nil {
+		t.Error(err)
+	}
+}
+
+func TestExportPublic(t *testing.T) {
+	docCrypto, err := NewCrypto()
+	if err != nil {
+		t.Error(err)
+	}
+	buf := &bytes.Buffer{}
+	err = docCrypto.ExportPublic(buf)
+	if err != nil {
+		t.Error(err)
+	}
+	fmt.Println(buf)
 }
